720a7981295473bebf3cb1455e300dacfedd6d59
Devin Ivy <devin@bigroomstudios.com>
2019-11-20 00:39:54 -0500

    Test toFactory() and compose.initialize

--- test/index.js
+++ test/index.js
@@ -21,9 +21,12 @@ describe('Ahem', () => {
         // A little guide behavioral guide to ahem:
         //
         // server    x controlled      = control-by:server  [and] register-to:new-server
+        //                                                  [and] initialize:server, not by default
         // server    x not-controlled  = control-by:n/a     [and] register-to:server (no server options allowed)
+        //                                                  [and] initialize:server, not by default
         // no-server x controlled      = (not-allowed)
         // no-server x not-controlled  = control-by:n/a     [and] register-to:new-server
+        //                                                  [and] initialize:new-server, by default
 
         it('instances a plugin.', async () => {
 
@@ -204,6 +207,74 @@ describe('Ahem', () => {
             expect(plugin).to.shallow.equal(srv);
         });
 
+        it('registers additional plugins using the compose.register option with unwrapped single plugin.', async () => {
+
+            let srv;
+
+            const plugin = await Ahem.instance({
+                name: 'my-plugin',
+                register: (server) => {
+
+                    srv = server;
+
+                    expect(server.registrations).to.only.include(['my-plugin', 'some-dependency']);
+                }
+            }, null, {
+                register: {
+                    name: 'some-dependency',
+                    register: () => null
+                }
+            });
+
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
+        });
+
+        it('registers additional plugins using the compose.register option with unwrapped multiple plugins.', async () => {
+
+            let srv;
+
+            const plugin = await Ahem.instance({
+                name: 'my-plugin',
+                register: (server) => {
+
+                    srv = server;
+
+                    expect(server.registrations).to.only.include(['my-plugin', 'some-dependency', 'some-other-dependency']);
+                    expect(server.realm.modifiers.route.prefix).to.not.exist();
+                }
+            }, null, {
+                register: [
+                    {
+                        plugin: {
+                            name: 'some-dependency',
+                            register: (server, options) => {
+
+                                expect(options).to.equal({
+                                    some: 'options'
+                                });
+
+                                expect(server.realm.modifiers.route.prefix).to.equal('/x');
+                            }
+                        },
+                        options: {
+                            some: 'options'
+                        },
+                        routes: {
+                            prefix: '/x'
+                        }
+                    },
+                    {
+                        name: 'some-other-dependency',
+                        register: () => null
+                    }
+                ]
+            });
+
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
+        });
+
         it('instances a plugin with a server provided compose.server options.', async () => {
 
             let srv;
@@ -354,24 +425,6 @@ describe('Ahem', () => {
             await expect(pluginPromise).to.reject('When a server is specified but compose.controlled is false, compose.server options are not allowed, as a new server will not be created.');
         });
 
-        it('decorates the instance\'s root server by default when controlled.', async () => {
-
-            const root = Hapi.server();
-            const plugin = await Ahem.instance(root, {
-                name: 'my-plugin',
-                register: (srv) => {
-
-                    expect(srv.root).to.not.exist();
-                }
-            }, null, {
-                controlled: true
-            });
-
-            expect(root.decorations.server).to.not.contain('root');
-            expect(plugin.decorations.server).to.contain('root');
-            expect(plugin.root.realm).to.shallow.equal(plugin.realm.parent);
-        });
-
         it('decorates the instance\'s root server by default when controlled by a passed server.', async () => {
 
             const server = Hapi.server();
@@ -517,6 +570,94 @@ describe('Ahem', () => {
 
             await expect(pluginPromise).to.reject('Cannot use compose.decorateRoot on a server that already has a different root decoration.');
         });
+
+        it('initializes instance by default when no server is specified.', async () => {
+
+            let initialized = false;
+
+            await Ahem.instance({
+                name: 'my-plugin',
+                register: (server) => {
+
+                    server.ext('onPreStart', () => {
+
+                        initialized = true;
+                    });
+                }
+            });
+
+            expect(initialized).to.equal(true);
+        });
+
+        it('doesn\'t initialize instance when no server is specified and controlled.initialize is false.', async () => {
+
+            let initialized = false;
+
+            await Ahem.instance({
+                name: 'my-plugin',
+                register: (server) => {
+
+                    server.ext('onPreStart', () => {
+
+                        initialized = true;
+                    });
+                }
+            }, null, {
+                initialize: false
+            });
+
+            expect(initialized).to.equal(false);
+        });
+
+        it('doesn\'t initialize instance by default when a server is specified.', async () => {
+
+            const initialized = { root: false, plugin: false };
+            const root = Hapi.server();
+
+            root.ext('onPreStart', () => {
+
+                initialized.root = true;
+            });
+
+            await Ahem.instance(root, {
+                name: 'my-plugin',
+                register: (server) => {
+
+                    server.ext('onPreStart', () => {
+
+                        initialized.plugin = true;
+                    });
+                }
+            });
+
+            expect(initialized).to.equal({ root: false, plugin: false });
+        });
+
+        it('initializes instance when a server is specified and compose.initialize is true.', async () => {
+
+            const initialized = { root: false, plugin: false };
+            const root = Hapi.server();
+
+            root.ext('onPreStart', () => {
+
+                initialized.root = true;
+            });
+
+            await Ahem.instance(root, {
+                name: 'my-plugin',
+                register: (server) => {
+
+                    server.ext('onPreStart', () => {
+
+                        initialized.plugin = true;
+                    });
+                }
+            }, null, {
+                initialize: true
+            });
+
+            expect(initialized).to.equal({ root: true, plugin: true });
+        });
     });
 
     describe('plugin', () => {
@@ -660,12 +801,12 @@ describe('Ahem', () => {
                 expect(root.app).to.equal({ some: 'value' });
             });
 
-            it('decorates the instance\'s root server by default when controlled.', async () => {
+            it('decorates the instance\'s root server by default when controlled by a passed server.', async () => {
 
-                const root = Hapi.server();
-                await root.register(Ahem);
+                const server = Hapi.server();
+                await server.register(Ahem);
 
-                const plugin = await root.instance({
+                const plugin = await server.instance({
                     name: 'my-plugin',
                     register: (srv) => {
 
@@ -675,10 +816,143 @@ describe('Ahem', () => {
                     controlled: true
                 });
 
-                expect(root.decorations.server).to.not.contain('root');
+                expect(server.decorations.server).to.not.contain('root');
                 expect(plugin.decorations.server).to.contain('root');
                 expect(plugin.root.realm).to.shallow.equal(plugin.realm.parent);
             });
+        });
+    });
+
+    describe('toFactory()', () => {
+
+        it('creates a function that instances a plugin.', async () => {
+
+            let srv;
+            let initialized = false;
+
+            const createPlugin = Ahem.toFactory({
+                name: 'my-plugin',
+                register: (server, options) => {
+
+                    srv = server;
+
+                    expect(options).to.equal({});
+
+                    server.ext('onPreStart', () => {
+
+                        initialized = true;
+                    });
+                }
+            });
+
+            const plugin = await createPlugin();
+
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
+            expect(initialized).to.equal(true);
+        });
+
+        it('creates a function that instances a plugin with additional arguments.', async () => {
+
+            let srv;
+            let initialized = false;
+
+            const createPlugin = Ahem.toFactory({
+                name: 'my-plugin',
+                register: (server, options) => {
+
+                    srv = server;
+
+                    expect(options).to.equal({ some: 'options' });
+
+                    server.ext('onPreStart', () => {
+
+                        initialized = true;
+                    });
+                }
+            });
+
+            const plugin = await createPlugin({ some: 'options' }, {
+                initialize: false
+            });
+
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
+            expect(initialized).to.equal(false);
+        });
+
+        it('creates a function that instances a plugin with a server.', async () => {
+
+            let srv;
+            const initialized = { root: false, plugin: false };
+
+            const createPlugin = Ahem.toFactory({
+                name: 'my-plugin',
+                register: (server, options) => {
+
+                    srv = server;
+
+                    expect(options).to.equal({});
+
+                    server.ext('onPreStart', () => {
+
+                        initialized.plugin = true;
+                    });
+                }
+            });
+
+            const root = Hapi.server();
+
+            root.ext('onPreStart', () => {
+
+                initialized.root = true;
+            });
+
+            const plugin = await createPlugin(root);
+
+            expect(initialized).to.equal({ root: false, plugin: false });
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
+
+            await root.initialize();
+
+            expect(initialized).to.equal({ root: true, plugin: true });
+        });
+
+        it('creates a function that instances a plugin with a server and additional arguments.', async () => {
+
+            let srv;
+            const initialized = { root: false, plugin: false };
+
+            const createPlugin = Ahem.toFactory({
+                name: 'my-plugin',
+                register: (server, options) => {
+
+                    srv = server;
+
+                    expect(options).to.equal({ some: 'options' });
+
+                    server.ext('onPreStart', () => {
+
+                        initialized.plugin = true;
+                    });
+                }
+            });
+
+            const root = Hapi.server();
+
+            root.ext('onPreStart', () => {
+
+                initialized.root = true;
+            });
+
+            const plugin = await createPlugin(root, { some: 'options' }, {
+                initialize: true
+            });
+
+            expect(initialized).to.equal({ root: true, plugin: true });
+            expect(plugin).to.exist();
+            expect(plugin).to.shallow.equal(srv);
         });
     });
 });
